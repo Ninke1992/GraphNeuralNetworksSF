87931cc69c1150514e793f6845f61bdf357b7984
Dijkstra <ninkebrenda.dijkstra@croonwolterendros.nl>
2022-09-02 17:03:02 +0200

    complete notebook 1

---
+++ src/functions.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+def visualize_emb(emb):
+  X = emb.weight.data.numpy()
+  pca = PCA(n_components=2)
+  components = pca.fit_transform(X)
+  plt.figure(figsize=(6, 6))
+  club1_x = []
+  club1_y = []
+  club2_x = []
+  club2_y = []
+  for node in G.nodes(data=True):
+    if node[1]['club'] == 'Mr. Hi':
+      club1_x.append(components[node[0]][0])
+      club1_y.append(components[node[0]][1])
+    else:
+      club2_x.append(components[node[0]][0])
+      club2_y.append(components[node[0]][1])
+  plt.scatter(club1_x, club1_y, color="red", label="Mr. Hi")
+  plt.scatter(club2_x, club2_y, color="blue", label="Officer")
+  plt.legend()
+  plt.show()
